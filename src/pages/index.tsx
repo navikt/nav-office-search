ddd9b837d7450979d3fed07815fb6f882c6d86e3
anders <anders@nomerstad.com>
2021-08-25 15:56:35 +0200

    Bruker SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,8 @@
 import { OfficeSearchApp } from '../components/OfficeSearchApp';
+import { GetServerSideProps } from 'next';
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    return { props: {} };
+};
 
 export default OfficeSearchApp;
