2a91fe63632c2e6a93fc59bb3405ef389c3f0a65
anders-nom <anders@nomerstad.com>
2021-08-26 13:20:49 +0200

    Fikser parametre i token fetch v2

---
+++ src/fetch/auth.ts
@@ -0,0 +1,29 @@
+import { fetchJson, objectToQueryString } from './utils';
+
+const tokenUrl = `https://login.microsoftonline.com/${process.env.AZURE_APP_TENANT_ID}/oauth2/v2.0/token`;
+
+const fetchToken = async () => {
+    const response = await fetchJson(tokenUrl, undefined, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: objectToQueryString({
+            grant_type: 'client_credentials',
+            client_id: process.env.AZURE_APP_CLIENT_ID,
+            client_secret: process.env.AZURE_APP_CLIENT_SECRET,
+            scope: `api://${process.env.API_CLIENT_ID}/.default`,
+        }),
+    });
+
+    fetch('asdf', {});
+    if (response.ok) {
+        return await response.json();
+    }
+
+    console.error('Bad response from token service', response);
+
+    throw new Error('Failed to fetch token!');
+};
+
+export const authPlaceholder = () => null;
