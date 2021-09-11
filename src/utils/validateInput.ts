b4e7e255f9fa95b7ce9eeb0f29506f284c05919e
anders <anders@nomerstad.com>
2021-09-11 21:19:19 +0200

    Fikser regex for server-side name validering

--- src/utils/validateInput.ts
+++ src/utils/validateInput.ts
@@ -1,9 +1,13 @@
+const postnrQueryRegex = new RegExp('^\\d{4}$');
+
 export const isValidPostnrQuery = (query: string) => {
     const [postnr] = query?.split(' ');
 
-    return postnr && /^\d{4}$/.test(postnr);
+    return postnr && postnrQueryRegex.test(postnr);
 };
 
+const nameQueryRegex = new RegExp('^(\\p{Letter}|\\.|-| ){2,}$', 'u');
+
 export const isValidNameQuery = (query: string) => {
-    return query && /^(\p{Letter}|\.|-| ){2,}$/gu.test(query);
+    return query && nameQueryRegex.test(query);
 };
