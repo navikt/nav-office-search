a36925030441696216c9757818e6533fc2168102
anders-nom <anders@nomerstad.com>
2021-10-04 16:00:32 +0200

    Bruker SSG + redirecter alt til index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,3 @@
 import { OfficeSearchApp } from '../components/OfficeSearchApp';
-import { GetServerSideProps } from 'next';
-
-export const getServerSideProps: GetServerSideProps = async () => {
-    return { props: {} };
-};
 
 export default OfficeSearchApp;
