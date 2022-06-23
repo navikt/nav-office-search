00c9a5fbc84bc9a5a25631097018aa13f3b8e1fe
Anders Bråthen Nomerstad <anders@nomerstad.com>
2022-06-23 15:42:39 +0200

    Oppdaterer next.js. Bruker SSG revalidate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
 import { OfficeSearchApp } from '../components/OfficeSearchApp';
+import { GetStaticProps } from "next";
+
+const getStaticProps: GetStaticProps = async () => {
+    return {
+        props: {},
+        revalidate: 300
+    };
+};
 
 export default OfficeSearchApp;
