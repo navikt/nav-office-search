cd54465887d65ccde00b13abe331b7edee446e43
Anders Bråthen Nomerstad <anders.brathen.nomerstad@nav.no>
2023-02-07 21:52:08 +0100

    Refactor chevron

--- src/components/OfficeLink/OfficeLink.tsx
+++ src/components/OfficeLink/OfficeLink.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BodyShort, Link } from '@navikt/ds-react';
-import { Chevron } from '../Chevron/Chevron';
 import { OfficeInfo } from '../../../common/types/data';
+import { OfficeLinkChevron } from './OfficeLinkChevron';
 
 import style from './OfficeLink.module.css';
 
@@ -14,7 +14,7 @@ export const OfficeLink = ({ officeInfo }: Props) => {
 
     return (
         <Link href={url} className={style.link}>
-            <Chevron className={style.chevron} />
+            <OfficeLinkChevron className={style.chevron} aria-hidden={true} />
             <BodyShort>{name}</BodyShort>
         </Link>
     );
