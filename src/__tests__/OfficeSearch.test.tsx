dcd91b13ca30c404ddc45381ad3e4b22f945e230
Skifjeld <eivind.skifjeld@nav.no>
2024-11-07 13:00:31 +0100

    Endret fra NAV til Nav i OfficeSearch.test.tsx

--- src/__tests__/OfficeSearch.test.tsx
+++ src/__tests__/OfficeSearch.test.tsx
@@ -41,28 +41,38 @@ describe('OfficeSearch', () => {
         fetch.mockResponse(JSON.stringify(errorInvalidPostnr), { status: 404 });
         searchForText('0000');
         await waitFor(() => {
-            expect(screen.getByText('Postnummeret finnes ikke')).toBeInTheDocument();
+            expect(
+                screen.getByText('Postnummeret finnes ikke')
+            ).toBeInTheDocument();
         });
     });
 
     test('gir valideringsfeil ved tomt input-felt', async () => {
         searchForText('');
         await waitFor(() => {
-            expect(screen.getByText('Skriv inn minst to bokstaver eller et postnummer')).toBeInTheDocument();
+            expect(
+                screen.getByText(
+                    'Skriv inn minst to bokstaver eller et postnummer'
+                )
+            ).toBeInTheDocument();
         });
     });
 
     test('gir valideringsfeil ved feil antall siffer', async () => {
         searchForText('11');
         await waitFor(() => {
-            expect(screen.getByText('Postnummer-søk må være fire siffer')).toBeInTheDocument();
+            expect(
+                screen.getByText('Postnummer-søk må være fire siffer')
+            ).toBeInTheDocument();
         });
     });
 
     test('gir valideringsfeil ved ugyldige tegn', async () => {
         searchForText('0354,.-');
         await waitFor(() => {
-            expect(screen.getByText('Søket inneholder ugyldige tegn')).toBeInTheDocument();
+            expect(
+                screen.getByText('Søket inneholder ugyldige tegn')
+            ).toBeInTheDocument();
         });
     });
 
@@ -70,7 +80,9 @@ describe('OfficeSearch', () => {
         fetch.mockResponse(JSON.stringify(postnrResultNone));
         searchForText('4737');
         await waitFor(() => {
-            expect(screen.getByText('Ingen NAV-kontor funnet for')).toBeInTheDocument();
+            expect(
+                screen.getByText('Ingen Nav-kontor funnet for')
+            ).toBeInTheDocument();
             expect(screen.getByText('4737 HORNNES')).toBeInTheDocument();
         });
     });
@@ -79,9 +91,11 @@ describe('OfficeSearch', () => {
         fetch.mockResponse(JSON.stringify(postnrResultOne));
         searchForText('4737');
         await waitFor(() => {
-            expect(screen.getByText('NAV-kontor for', {exact: false})).toBeInTheDocument();
+            expect(
+                screen.getByText('Nav-kontor for', { exact: false })
+            ).toBeInTheDocument();
             expect(screen.getByText('4737 HORNNES')).toBeInTheDocument();
-            expect(getLinkByName('NAV Evje og Hornnes')).toBeInTheDocument();
+            expect(getLinkByName('Nav Evje og Hornnes')).toBeInTheDocument();
         });
     });
 
@@ -89,14 +103,18 @@ describe('OfficeSearch', () => {
         fetch.mockResponse(JSON.stringify(postnrResultMany));
         searchForText('4737');
         await waitFor(() => {
-            expect(screen.getByText('2 kontorer dekker', {exact: false})).toBeInTheDocument();
+            expect(
+                screen.getByText('2 kontorer dekker', { exact: false })
+            ).toBeInTheDocument();
             expect(screen.getByText('4737 HORNNES')).toBeInTheDocument();
-            expect(screen.getByText(
-                'Du kan legge til gatenavn og husnummer for å spisse søket, f.eks. 4737 Eksempelgata 12',
-                {exact: false}
-            )).toBeInTheDocument();
-            expect(getLinkByName('NAV Evje og Hornnes')).toBeInTheDocument();
-            expect(getLinkByName('NAV Testkontor')).toBeInTheDocument();
+            expect(
+                screen.getByText(
+                    'Du kan legge til gatenavn og husnummer for å spisse søket, f.eks. 4737 Eksempelgata 12',
+                    { exact: false }
+                )
+            ).toBeInTheDocument();
+            expect(getLinkByName('Nav Evje og Hornnes')).toBeInTheDocument();
+            expect(getLinkByName('Nav Testkontor')).toBeInTheDocument();
         });
     });
 
@@ -104,12 +122,20 @@ describe('OfficeSearch', () => {
         fetch.mockResponse(JSON.stringify(postnrResultPostbox));
         searchForText('0614');
         await waitFor(() => {
-            expect(screen.getByText('0614 er et postnummer for postbokser i', {exact: false})).toBeInTheDocument();
+            expect(
+                screen.getByText('0614 er et postnummer for postbokser i', {
+                    exact: false,
+                })
+            ).toBeInTheDocument();
             expect(screen.getByText('OSLO')).toBeInTheDocument();
-            expect(screen.getByText('kommune. Kommunens NAV-kontorer:', {exact: false})).toBeInTheDocument();
-            expect(getLinkByName('NAV Alna')).toBeInTheDocument();
-            expect(getLinkByName('NAV Bjerke')).toBeInTheDocument();
-            expect(getLinkByName('NAV Frogner')).toBeInTheDocument();
+            expect(
+                screen.getByText('kommune. Kommunens Nav-kontorer:', {
+                    exact: false,
+                })
+            ).toBeInTheDocument();
+            expect(getLinkByName('Nav Alna')).toBeInTheDocument();
+            expect(getLinkByName('Nav Bjerke')).toBeInTheDocument();
+            expect(getLinkByName('Nav Frogner')).toBeInTheDocument();
         });
     });
 
@@ -117,12 +143,20 @@ describe('OfficeSearch', () => {
         fetch.mockResponse(JSON.stringify(postnrResultServicebox));
         searchForText('0614');
         await waitFor(() => {
-            expect(screen.getByText('0614 er et servicepostnummer i', {exact: false})).toBeInTheDocument();
+            expect(
+                screen.getByText('0614 er et servicepostnummer i', {
+                    exact: false,
+                })
+            ).toBeInTheDocument();
             expect(screen.getByText('OSLO')).toBeInTheDocument();
-            expect(screen.getByText('kommune. Kommunens NAV-kontorer:', {exact: false})).toBeInTheDocument();
-            expect(getLinkByName('NAV Alna')).toBeInTheDocument();
-            expect(getLinkByName('NAV Bjerke')).toBeInTheDocument();
-            expect(getLinkByName('NAV Frogner')).toBeInTheDocument();
+            expect(
+                screen.getByText('kommune. Kommunens Nav-kontorer:', {
+                    exact: false,
+                })
+            ).toBeInTheDocument();
+            expect(getLinkByName('Nav Alna')).toBeInTheDocument();
+            expect(getLinkByName('Nav Bjerke')).toBeInTheDocument();
+            expect(getLinkByName('Nav Frogner')).toBeInTheDocument();
         });
     });
 
@@ -130,12 +164,20 @@ describe('OfficeSearch', () => {
         fetch.mockResponse(JSON.stringify(postnrResultBydel));
         searchForText('0354');
         await waitFor(() => {
-            expect(screen.getByText('Fant ingen kontor spesifikt tilknyttet', {exact: false})).toBeInTheDocument();
+            expect(
+                screen.getByText('Fant ingen kontor spesifikt tilknyttet', {
+                    exact: false,
+                })
+            ).toBeInTheDocument();
             expect(screen.getByText('0354')).toBeInTheDocument();
-            expect(screen.getByText('Alle kommunens NAV-kontorer:', {exact: false})).toBeInTheDocument();
-            expect(getLinkByName('NAV Alna')).toBeInTheDocument();
-            expect(getLinkByName('NAV Bjerke')).toBeInTheDocument();
-            expect(getLinkByName('NAV Frogner')).toBeInTheDocument();
+            expect(
+                screen.getByText('Alle kommunens Nav-kontorer:', {
+                    exact: false,
+                })
+            ).toBeInTheDocument();
+            expect(getLinkByName('Nav Alna')).toBeInTheDocument();
+            expect(getLinkByName('Nav Bjerke')).toBeInTheDocument();
+            expect(getLinkByName('Nav Frogner')).toBeInTheDocument();
         });
     });
 
@@ -143,8 +185,10 @@ describe('OfficeSearch', () => {
         fetch.mockResponse(JSON.stringify(stedsnavnResultWithHits));
         searchForText('evje og hornnes');
         await waitFor(() => {
-            expect(screen.getByText('Søkeresultat for "evje og hornnes" (1):')).toBeInTheDocument();
-            expect(getLinkByName('NAV Evje og Hornnes')).toBeInTheDocument();
+            expect(
+                screen.getByText('Søkeresultat for "evje og hornnes" (1):')
+            ).toBeInTheDocument();
+            expect(getLinkByName('Nav Evje og Hornnes')).toBeInTheDocument();
         });
     });
 
@@ -152,7 +196,9 @@ describe('OfficeSearch', () => {
         fetch.mockResponse(JSON.stringify(stedsnavnResultWithoutHits));
         searchForText('evje og hornnes');
         await waitFor(() => {
-            expect(screen.getByText('Ingen resultater for "evje og hornnes"')).toBeInTheDocument();
+            expect(
+                screen.getByText('Ingen resultater for "evje og hornnes"')
+            ).toBeInTheDocument();
         });
     });
 });
