e088dbcafc9e2cec3d8feed0bb9741bde868a50a
Skifjeld <eivind.skifjeld@nav.no>
2026-01-12 15:25:05 +0100

    Oppdatert avhengigheter 120126

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
-import compression from 'compression';
 import { registerSiteRoutes } from './site/registerSiteRoutes.js';
 import { registerApiRoutes } from './api/registerApiRoutes';
 import { loadDataAndStartSchedule } from './data/data';
@@ -17,7 +16,6 @@ const app = express();
 const siteRouter = express.Router();
 const apiRouter = express.Router();
 
-app.use('*', compression());
 app.use(basePath, siteRouter);
 siteRouter.use('/api', apiRouter);
 
