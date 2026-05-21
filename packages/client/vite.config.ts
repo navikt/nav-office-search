000deaffbd26c8403d8be74461ac81b004a63e9f
Terje Karlsen <terje@terjeofnorway.no>
2026-05-21 13:02:51 +0200

    Sletter duplikat vite.config

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,9 +1,14 @@
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig, loadEnv } from 'vite';
 import preact from '@preact/preset-vite';
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+    const rootDir = resolve(__dirname, '../..');
+    const env = loadEnv(mode, rootDir);
 
     return {
         plugins: [preact()],
@@ -18,6 +23,6 @@ export default defineConfig(({ mode }) => {
             // and does not depend on server node_modules for client packages.
             noExternal: true,
         },
-        base: process.env.VITE_APP_BASEPATH,
+        base: env.VITE_APP_BASEPATH,
     };
 });
