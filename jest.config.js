e45786ec94c48e1b5f20fc8a8701ecb388047fbe
Tania Holst <tania.holst@nav.no>
2026-05-29 12:52:43 +0200

    Oppgraderer til TS6 (#204)

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,24 @@ export default {
     moduleNameMapper: {
         '.+\\.(css)$': 'identity-obj-proxy',
     },
+    transform: {
+        '^.+\\.tsx?$': [
+            'ts-jest',
+            {
+                tsconfig: {
+                    module: 'CommonJS',
+                    moduleResolution: 'node10',
+                    esModuleInterop: true,
+                    jsx: 'react-jsx',
+                    target: 'ES2022',
+                    isolatedModules: true,
+                    resolveJsonModule: true,
+                    skipLibCheck: true,
+                    strict: true,
+                    types: ['jest', '@testing-library/jest-dom'],
+                    ignoreDeprecations: '6.0',
+                },
+            },
+        ],
+    },
 };
